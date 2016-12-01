36fee24c6a0c19a61250800cc11b8c1f6e4b6b7d
miz37 <miz37@pitt.edu>
2016-12-01 17:21:34 -0500

    print suggestions

--- assignment4.js
+++ assignment4.js
@@ -11,6 +11,51 @@
 // $.html(), $.text(), etc.
 // keyup events could be helpful to get value of field as the user types
 
+function showSuggestions(str){
+	// var xhttp;
+	if(str.length == 0){
+		document.getElementById("suggestions").innerHTML = "nothing";
+	}
+	// xhttp = new XMLHttpRequest();
+	// xhttp.onreadystatechange = function(){
+
+	// 	if(this.readyState == 4 && this.status == 200){
+	// 		var json = JSON.parse(xhttp.responseText);
+	// 		document.getElementById("txtHint").innerHTML = json.interests;
+	// 	}
+	// };
+
+	// xhttp.open("GET", "http://www.mattbowytz.com/simple_api.json?data="+str, true);
+	// xhttp.send();
+
+	$.ajax({
+		type: 'GET',
+		url: "http://www.mattbowytz.com/simple_api.json?data=all",
+		async: true,
+		dataType: 'json',
+		success: function(result){
+			var values = [];
+			var suggestion = "";
+			var suggestions = $('#suggestions');
+			$.each(result.data, function(key, value){
+				//console.log(key + ' is ' + value);
+				$.each(value, function(key, value){
+					//console.log(value);
+					if(value.startsWith(str)){
+						suggestion += values;
+						console.log(value);
+						suggestions.append(value);
+					}
+				})
+			})
+
+			suggestions.empty();
+			
+		}
+	});
+
+}
+
 (function() {
   // Magic!
   console.log('Keepin\'n it clean with an external script!');
